532b88764056c0d3eb461cfa333e79375e81e7f1
ashudeore8767 <ashleshadeore11@gmail.com>
2024-08-30 13:28:32 +0530

    Add files via upload

---
+++ areaof_rect.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class areaof_rect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int length = 5; 
+		int width = 2; 
+		int area = length * width; 
+		System.out.println("Area of rectangle: " + area); 
+
+		
+	}
+
+}
